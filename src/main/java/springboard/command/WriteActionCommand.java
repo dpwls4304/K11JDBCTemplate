14f121a608f3a8c11be6471b3395353015f55748
tego14 <tego14@naver.com>
2020-06-25 18:32:07 +0900

    06/25

---
+++ src/main/java/springboard/command/WriteActionCommand.java
@@ -0,0 +1,31 @@
+package springboard.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+import springboard.model.JDBCTemplateDAO;
+import springboard.model.SpringBbsDTO;
+
+public class WriteActionCommand implements BbsCommandImpl{
+	
+	@Override
+	public void execute(Model model) {
+		Map<String, Object> paramMap = model.asMap();
+		HttpServletRequest req = 
+				(HttpServletRequest)paramMap.get("req");
+		SpringBbsDTO springBbsDTO =
+				(SpringBbsDTO)paramMap.get("springBbsDTO");
+		
+		//커맨드객체에 저장된 title을 로그에 출력(폼값확인용)
+		System.out.println("springBbsDTO.title=" + springBbsDTO.getTitle());
+		
+		//DAO객체 생성후 insert처리
+		JDBCTemplateDAO dao = new JDBCTemplateDAO();
+		
+		dao.write(springBbsDTO);
+		dao.close();
+	}
+}
